d8c8e0394b68b38e9fd7a1dbfb02db19be258855
Aman Yadav <yourgithubemail@gmail.com>
2026-06-16 21:37:49 +0530

    Added day 6 question 2

---
+++ Day-06/q2.cpp
@@ -0,0 +1,23 @@
+// Code for binary to decimal :
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    long long binary;
+    cout<<"Enter a binary no.:";
+    cin >> binary;
+
+    int decimal = 0, i = 0;
+
+    while(binary > 0) {
+        int digit = binary % 10;
+        decimal += digit * pow(2, i);
+        binary /= 10;
+        i++;
+    }
+
+    cout << decimal;
+
+    return 0;
+}
